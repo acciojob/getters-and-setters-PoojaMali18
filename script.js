08c2d1c013caf9fe75d041b547faa243122b604c
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-11-19 01:13:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //complete this code
-class Person {}
+class Person {
+	constructor details(name,age)
+	{
+		this.name = name;
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor study()
+	{
+		super(name,age);
+		console.log(name +"is studying")
+	}
+	
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor teach()
+	{
+		super(name,age);
+		console.log(name +"is taeching")
+	}
+	
+}
 
 // Do not change the code below this line
 window.Person = Person;
